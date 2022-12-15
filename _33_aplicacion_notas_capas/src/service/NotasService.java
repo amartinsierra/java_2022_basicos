a8dcc4849af3f9531e67d28c3555f2634bca95d7
Administrator <Administrator@A13E100>
2022-12-15 13:22:46 +0100

    commit inicial 15-12-2022

---
+++ _33_aplicacion_notas_capas/src/service/NotasService.java
@@ -0,0 +1,71 @@
+package service;
+
+import java.util.ArrayList;
+
+public class NotasService {
+	ArrayList<Double> notas=new ArrayList<>();
+	
+	public void guardarNota(double nota){
+		notas.add(nota);
+	}
+	
+	public double media(){
+		double media=0;
+		for(int i=0;i<notas.size();i++) {
+			media=media+notas.get(i);
+		}
+		media=media/notas.size();
+		return media;
+	}
+	
+	public int aprobados() {
+		int aprobados=0;
+		for(int i=0;i<notas.size();i++) {
+			if(notas.get(i)>=5) {
+				aprobados++;
+			}
+		}
+		return aprobados;
+	}
+	
+	public double notaMax() {
+		double notaMax=notas.get(0);
+		for(int i=0;i<notas.size();i++) {
+			notaMax=Math.max(notaMax, notas.get(i));
+		}
+		return notaMax;	
+	}
+	
+	public double notaMin() {
+		double notaMin=notas.get(0);
+		for(int i=0;i<notas.size();i++) {
+			notaMin=Math.min(notaMin, notas.get(i));
+		}
+		return notaMin;	
+	}
+	
+	public double[] notas() {
+		double[] res=new double[notas.size()];
+		for(int i=0;i<notas.size();i++) {
+			res[i]=notas.get(i);
+		}
+		return res;
+	}
+	
+	public void eliminarSuspensos() {
+		for(int i=0;i<notas.size();i++) {
+			//si es suspenso la borramos
+			if(notas.get(i)<5) {
+				notas.remove(i);
+			}
+		}
+	}
+	/*public ArrayList<Double> notas(){
+		return notas;
+	}
+	
+	public List<Double> notas(){
+		return notas;
+	}
+	*/
+}
---
+++ _33_aplicacion_notas_capas/src/view/NotasMain.java
@@ -0,0 +1,72 @@
+package view;
+
+import java.util.Scanner;
+
+import service.NotasService;
+
+public class NotasMain {
+	static NotasService notasService=new NotasService();
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int opcion;
+		do {
+			mostrarMenu();
+			opcion=sc.nextInt();//lee la opción elegida
+			switch(opcion) { //evaluamos
+				case 1:
+					pedirNota();
+					break;
+				case 2:
+					mostrarMedia();
+					break;
+				case 3:
+					mostrarAprobados();
+					break;
+				case 4:
+					mostrarExtremas();
+					break;
+				case 5:
+					mostrarTodas();
+					break;
+				case 6:
+					eliminarSuspensos();
+			}
+		}while(opcion!=7);
+
+	}
+	static void mostrarMenu() {
+		System.out.println("1.- Grabar nota");
+		System.out.println("2.- Calcular media");
+		System.out.println("3.- Aprobados");
+		System.out.println("4.- Notas extremas");
+		System.out.println("5.- Mostrar todas");
+		System.out.println("6.- Eliminar suspensos");
+		System.out.println("7.- Salir");
+	}
+	static void pedirNota() {
+		Scanner sc=new Scanner(System.in);
+		double nota;
+		System.out.println("Introduce nota");
+		nota=sc.nextDouble();
+		notasService.guardarNota(nota);
+	}
+	static void mostrarMedia() {
+		System.out.println("La media es: "+notasService.media());
+	}
+	static void mostrarAprobados() {
+		System.out.println("Aprobados: "+notasService.aprobados());
+	}
+	static void mostrarExtremas() {
+		System.out.println("Nota máxima: "+notasService.notaMax());
+		System.out.println("Nota mínima: "+notasService.notaMin());
+	}
+	static void mostrarTodas() {
+		double[] notas=notasService.notas();
+		for(int i=0;i<notas.length;i++) {
+			System.out.println(notas[i]);
+		}
+	}
+	static void eliminarSuspensos() {
+		notasService.eliminarSuspensos();
+	}
+}
